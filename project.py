d1cf1014714634a68e6af83d9d7b1bd1df0e9c93
anushriiyer <anushriiiyer@gmail.com>
2024-11-11 14:16:17 +0800

    update

--- project.py
+++ project.py
@@ -1,11 +1,19 @@
 from interface import Interface
+<<<<<<< HEAD
 #from preprocessing import Preprocessing
 #from whatif import WhatIf
+=======
+
+# from preprocessing import Preprocessing
+
+from whatif import WhatIf
+>>>>>>> 777c8ce86c01011e80c9c68680c910b10e7bb198
 
 
 class Project:
     def __init__(self):
         self.interface = Interface()
+<<<<<<< HEAD
         #self.preprocessing = Preprocessing()
         #self.whatif = WhatIf()
 
@@ -13,6 +21,16 @@ class Project:
         self.interface.run()
         #self.preprocessing.run()
         #self.whatif.run()
+=======
+        # self.preprocessing = Preprocessing()
+
+        self.whatif = WhatIf()
+
+    def run(self):
+        self.interface.run()
+        # self.preprocessing.run()
+        self.whatif.run()
+>>>>>>> 777c8ce86c01011e80c9c68680c910b10e7bb198
 
 
 if __name__ == '__main__':
